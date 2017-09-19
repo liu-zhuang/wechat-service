05a842454cd9960d9953564ea6eb0153bf0ddf74
nickylau82 <70458055@qq.com>
2017-09-19 23:36:23 +0800

    modify port

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Sha = require('sha1');
+const route = require('koa-route');
 
 const app = new Koa();
 
@@ -7,8 +8,33 @@ const main = ctx => {
 	ctx.response.type = 'html';
 	ctx.response.body = '<h1>wechat</h1>'
 }
+const config = {
+	token: 'yebaomemeda'
+};
 
-app.use(main);
+const check = ctx => {
+	//console.log('query:'+ctx.query);
+	// console.log('nonce:'+ctx.query.nonce);
+	console.log(ctx.query.signature);
+	let signature = ctx.query.signature;
+	let timestamp = ctx.query.timestamp;
+	let nonce = ctx.query.nonce;
+	let echostr = ctx.query.echostr;
+	let sort = [config.token, timestamp, nonce].sort().join();
+	let sha1 = Sha(sort);
+	console.log(sha1);
+	if (sha1 === signature) {
+		console.log('success');
+		ctx.response.body = echostr;
+	} else {
+		console.log('fail');
+		ctx.response.body = 'wrong';
+	}
+}
+
+
+app.use(route.get('/', main));
+app.use(route.get('/check', check));
 
 app.listen(7001);
 
